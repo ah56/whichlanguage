84835b5dbb9d832c9bcec8da755ab7930b65c973
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-08-16 16:31:42 -0700

    this git adds a working js which will display the languages you speak

--- java/java.js
+++ java/java.js
@@ -2,24 +2,36 @@ $(document).ready(function(){
   alert("the fire");
 $('#someform').submit(function(event){
   alert("the second fire");
-  var imputBug = $("#bug").val();
+  var imputBug = parseInt($("select#bug").val());
   if (imputBug= 0){
-    $('#js').show();
+    $(".jss").append("You Speak JS!");
   };
   alert("the third fire");
-  var imputBird= $("#bird").val();
-  if (imputBird= 0){
-    $('#js').show();
+  var imputBird= parseInt($("select#bird").val());
+  if (imputBird= 1){
+    $(".jss").text("You Speak JS!");
   };
   alert("the forth fire");
-  var imputCat=$("#cat").val();
+  var imputCat=parseInt($("select#cat").val());
   if (imputBird= 0){
-    $('#js').show();
+    $('.jss').show();
+  };
+  var imputBlackKeys=parseInt($("select#blackkeys").val());
+  if (imputBlackKeys= 1){
+    $('.python').show();
+  };
+  var imputIndiferent=parseInt($("select#indiferent").val());
+  if (imputIndiferent= 0){
+    $('.python').show();
+  };
+  var imputDiamond=parseInt($("select#diamond").val());
+  if (imputDiamond= 0){
+    $('.go').show();
   };
   event.preventDefault();
-  alert("the second fire");
+  alert("the fifth fire");
 });
-/*$("button").click(function(){
+$("button").click(function(){
 $("button").fadeOut();
-});*/
+});
 });
