19b95b654a9cc15bfae8fcca43738809c39570d5
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-08-16 13:30:13 -0700

    this commit creates another branch and continues to work on js

--- java/java.js
+++ java/java.js
@@ -1,31 +1,10 @@
-var yourLanguage = funciton(val("#bug"), val("#bird"), val("#cat"), val("#blackkeys"), val("#indiferent"), val("#diamond"), val("#fly")){
- if(val(#bug)="yes"{
-  var language = "Rust";
-});
-else if (#bird="yes"){
-  var language = "JavaScript";
-});
-else if (val(#cat)="yes"){
-  var language = "Python";
-};
-else if (val(#blackkeys)="yes"){
-  var language ="C#";
-};
-else if (val(#indiferent)="yes"){
-  var language ="Go";
-};
-else if (val(#diamond)="yes"){
-  var language = "Ruby";
-};
-else{
-var language = "Naught";
-return language;
-};
-};
-
 $(document).ready(function(){
-$("form#btton").submit(function(event){
+("btn").submit(function(event){
+  var yourLanguage= "alkdjfldsajkfl";
+  $("p#output").text(yourLanguage);
 event.preventDefault();
-$("#output").text(yourLanguage);
 });
+/*$("button").click(function(){
+$("button").fadeOut();
+});*/
 });
